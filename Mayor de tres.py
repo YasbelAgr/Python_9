90676a51a5e36878248fa19f2ca8bec4aa096c15
YasbelAgr <yasbelnac06@gmail.com>
2024-11-24 19:49:36 -0600

    first commit

---
+++ Mayor de tres.py
@@ -0,0 +1,8 @@
+
+num1 = int(input("Introduce el primer numero"))
+num2 = int(input("Introduce el segundo numero"))
+num3 = int(input("Introduce el tercer numero"))
+
+mayor = max(num1, num2, num3)
+
+print(f"El numero mayor es {mayor}")
